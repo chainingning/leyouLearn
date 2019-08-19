2f5ebb96a62a718b8e70e862495448114d9fbb08
ningning.chai <>
2019-08-19 10:22:35 +0800

    修改服务提供者和消费者的application的name，初步使用eureka

--- chai-service-consumer/src/main/java/cn/chai/service/controller/UserController.java
+++ chai-service-consumer/src/main/java/cn/chai/service/controller/UserController.java
@@ -1,13 +1,18 @@
 package cn.chai.service.controller;
 
 import cn.chai.service.pojo.User;
+import com.netflix.discovery.converters.Auto;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 /**
  * @ClassName UserController
@@ -23,9 +28,15 @@ public class UserController {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
     @GetMapping
     @ResponseBody
     public User queryUserById(@RequestParam("id")Long id){
-        return this.restTemplate.getForObject("http://localhost:8081/user/"+id,User.class);
+        List<ServiceInstance> instances = discoveryClient.getInstances("service-provider");
+        ServiceInstance serviceInstance = instances.get(0);
+
+        return this.restTemplate.getForObject("http://"+serviceInstance.getHost()+":"+serviceInstance.getPort()+"/user/"+id,User.class);
     }
 }
