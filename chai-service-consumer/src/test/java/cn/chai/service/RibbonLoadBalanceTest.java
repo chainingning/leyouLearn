c6d8c2d8e6b911f68b027bc4c480f0d4bb282bb6
ningning.chai <>
2019-08-22 10:44:23 +0800

    robbin负载均衡

---
+++ chai-service-consumer/src/test/java/cn/chai/service/RibbonLoadBalanceTest.java
@@ -0,0 +1,11 @@
+package cn.chai.service;
+
+/**
+ * @ClassName RibbonLoadBalanceTest
+ * @Description: xxx
+ * @Author ningning.chai
+ * @Date 2019/8/22
+ * @Version V1.0
+ **/
+public class RibbonLoadBalanceTest {
+}
