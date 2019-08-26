024648084405255fec3da644ad8751beeea20f7f
ningning.chai <>
2019-08-26 10:48:39 +0800

    springcloudApplication组合注解

--- chai-service-consumer/src/main/java/cn/chai/service/ChaiServiceConsumerApplication.java
+++ chai-service-consumer/src/main/java/cn/chai/service/ChaiServiceConsumerApplication.java
@@ -2,15 +2,17 @@ package cn.chai.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-@SpringBootApplication
-@EnableDiscoveryClient
-@EnableCircuitBreaker//开启熔断
+//@SpringBootApplication
+//@EnableDiscoveryClient
+//@EnableCircuitBreaker//开启熔断
+@SpringCloudApplication
 public class ChaiServiceConsumerApplication {
 
     @Bean
